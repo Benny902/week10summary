558648f0769c0e17266e6e34450abfca0765e787
Benny <Benny902@gmail.com>
2025-07-08 14:23:24 +0300

    b

--- backend.tf
+++ backend.tf
@@ -1,8 +1,3 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "tfstate-backend-rg"
-    storage_account_name = "tfstateweek10infra"
-    container_name       = "tfstate"
-    key                  = "dev.tfstate"
-  }
+  backend "azurerm" {}
 }
