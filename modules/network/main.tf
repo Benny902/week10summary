bc4e49e3e719d9bf61cda10dc1ad2820fad7de03
Benny <Benny902@gmail.com>
2025-07-08 13:55:11 +0300

    static ip

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -71,6 +71,7 @@ resource "azurerm_network_interface" "nic" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Static"
+    private_ip_address            = "10.0.1.4"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
